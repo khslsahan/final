31eb0599f5d8b11a4998860dea95707c7fdc8534
unknown <gmgpathum@gmail.com>
2020-10-21 12:24:46 +0530

    mySecond

--- src/components/hedfoo/Header.vue
+++ src/components/hedfoo/Header.vue
@@ -1,12 +1,12 @@
 <template>
-<div id="app">
-    <router-view />
-</div>
+<header>
+    <h1>National Elders Allawance System</h1>
+</header>
 </template>
 
 <script>
 export default {
-    name: 'App'
+
 }
 </script>
 
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,28 @@
 <template>
-<header>Login</header>
+<div>
+    <Header />
+    <img id="bgimage" src="../assets/login.jpg">
+    <Footer />
+
+</div>
 </template>
 
 <script>
+import Header from './hedfoo/Header';
+import Footer from './hedfoo/Footer';
+
 export default {
+    components: {
+        'Header': Header,
+        'Footer': Footer
+    }
 
 }
 </script>
 
 <style>
-
+#bgimage {
+    width: 100%;
+    height: 400px;
+}
 </style>
